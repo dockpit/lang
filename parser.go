e3899ccccb13a41504563633b5dfd6b660abdd88
Ad van der Veer <advanderveer@gmail.com>
2014-11-27 12:55:26 +0100

    debugging...

--- parser.go
+++ parser.go
@@ -431,7 +431,7 @@ func (p *Parser) visit(fpath string, fi os.FileInfo, err error) error {
 					return err
 				}
 
-				fmt.Println("AAAAAA", p.currentCase, p.currentCase.Then, then)
+				fmt.Println("AAAAAA", p.currentCase, then)
 				p.currentCase.Then = *then
 			} else if filepath.Base(fpath) == "while" {
 				whiles, err := p.ParseWhile(f, fpath)
